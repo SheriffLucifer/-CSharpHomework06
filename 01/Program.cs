f71bece2ce1840074a3926b172e7033aa9f974f5
Oleg <olegashepelev@gmail.com>
2023-01-21 18:46:13 +0600

    Complete first task

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿Console.Clear();
+
+Console.Write("Введите числа через запятую: ");
+int[] array = NumbersToArray(Console.ReadLine());
+int numbersGreaterZero = ShowNumbersGreaterZero(array);
+Console.WriteLine($"[{String.Join(", ", array)}] -> {numbersGreaterZero}");
+
+int[] NumbersToArray(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int[count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input[i] != ',')
+        {
+            if (i != input.Length - 1)
+            {
+                temp += input[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input[i].ToString();
+                break;
+            }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+
+int ShowNumbersGreaterZero(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
